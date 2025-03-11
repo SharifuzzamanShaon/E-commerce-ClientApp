47e89d6d7caa05a4a74e40ab4877bf4952ed9945
sharifuzzamanshaon <shasan2200@gmail.com>
2025-03-11 16:33:23 +0600

    Configure Next.js to allow images from specified domains in environment variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS
+          ? process.env.NEXT_PUBLIC_IMAGE_DOMAINS.split(",")
+          : [],
+      },
+};
 
 module.exports = nextConfig;

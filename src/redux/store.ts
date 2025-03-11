b5cae9ce269aba48749d32f3794ba281678b706f
sharifuzzamanshaon <shasan2200@gmail.com>
2025-03-11 13:04:05 +0600

    Add apiSlice to Redux store for enhanced API handling

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import quickViewReducer from "./features/quickView-slice";
 import cartReducer from "./features/cart-slice";
 import wishlistReducer from "./features/wishlist-slice";
 import productDetailsReducer from "./features/product-details";
-
 import { TypedUseSelectorHook, useSelector } from "react-redux";
+import apiSlice from "./features/api/apiSlice";
 
 export const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     quickViewReducer,
     cartReducer,
     wishlistReducer,
     productDetailsReducer,
   },
-});
+  middleware: (getDefaultMiddleware):any=>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+    
+})
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
